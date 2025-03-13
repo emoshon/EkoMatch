4d647e10002e83ec9b2b48d9eeff33506949b4ef
emoshon <rukiye.dislioglu@stu.khas.edu.tr>
2025-03-14 00:58:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("login-btn").addEventListener("click", function () {
+        alert("Giriş sayfasına yönlendiriliyorsunuz...");
+    });
+});
